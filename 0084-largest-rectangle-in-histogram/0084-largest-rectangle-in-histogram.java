6b5ae268db6737f697c2c2bc6d28db8f31de410d
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2025-08-25 09:21:58 +0530

    Time: 119 ms (8.26%), Space: 61.5 MB (5.21%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,6 +1,6 @@
 class Solution {
-    //Previous Smaller Element
-    public static int[] prevSmallerElement(int[] nums) {
+    //Previous Smaller Height
+    public static int[] prevSmallerHeight(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
         Stack<Integer> st = new Stack<>();
@@ -15,8 +15,8 @@ class Solution {
         return res;
     }
 
-    //Next Smaller Element
-    public static int[] nextSmallerElement(int[] nums) {
+    //Next Smaller height
+    public static int[] nextSmallerHeight(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
         Stack<Integer> st = new Stack<>();
@@ -32,25 +32,8 @@ class Solution {
     }
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int[] prevSmaller = prevSmallerElement(heights);
-        int[] nextSmaller = nextSmallerElement(heights);
-        Stack<Integer> stack = new Stack<>();
-
-        // Nearest Smaller to Left
-        // for (int i = 0; i < n; i++) {
-        //     while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
-        //     left[i] = stack.isEmpty() ? -1 : stack.peek();
-        //     stack.push(i);
-        // }
-
-        //stack.clear(); // Reuse stack
-
-        // Nearest Smaller to Right
-        // for (int i = n - 1; i >= 0; i--) {
-        //     while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
-        //     right[i] = stack.isEmpty() ? n : stack.peek();
-        //     stack.push(i);
-        // }
+        int[] prevSmaller = prevSmallerHeight(heights);
+        int[] nextSmaller = nextSmallerHeight(heights);
 
         int maxArea = 0;
         for (int i = 0; i < n; i++) {
