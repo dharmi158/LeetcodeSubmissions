c22293f0e728fd31e3683eccf624badc34b2db28
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2025-08-28 23:47:36 +0530

    Time: 5 ms (99.16%), Space: 46.2 MB (21.64%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static boolean binarySearch(int[] matrix, int target){
+    //public static boolean binarySearch(int[] matrix, int target){
 
         // Naive Solution  - TC - O(N * M), SC- O(1)
         // int n = matrix.length;
@@ -14,31 +14,31 @@ class Solution {
         // return false;
 
         //Optimal Solution TC - O(N * logM), SC - O(1)
-        int n = matrix.length;
-        int left =0, right = n-1;
-        while(left<=right){
-            int mid = (left + right)/2;
-            if(matrix[mid] == target){
-                return true;
-            }
-            if(matrix[mid]<target){
-                left = mid+1;
-            }
-            else{
-                right = mid-1;
-            }
-        }
-        return false;
-    }
+        // int n = matrix.length;
+        // int left =0, right = n-1;
+        // while(left<=right){
+        //     int mid = (left + right)/2;
+        //     if(matrix[mid] == target){
+        //         return true;
+        //     }
+        //     if(matrix[mid]<target){
+        //         left = mid+1;
+        //     }
+        //     else{
+        //         right = mid-1;
+        //     }
+        // }
+        // return false;
+    //}
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        for(int i=0;i<n;i++){
-            boolean ans =  binarySearch(matrix[i],target);
-            if (ans == true)
-                return true;
-        }
-        return false;
+        // int n = matrix.length;
+        // int m = matrix[0].length;
+        // for(int i=0;i<n;i++){
+        //     boolean ans =  binarySearch(matrix[i],target);
+        //     if (ans == true)
+        //         return true;
+        // }
+        // return false;
 
 
         //2nd Optimal Solution  TC - O(N + M), SC - O(1) - efficient code
@@ -55,5 +55,26 @@ class Solution {
         //     }
         // }
         // return false;
+
+        //                       OR
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = n - 1;
+        int col = 0;
+        while(row >= 0 && col<m)
+        {
+            if (matrix[row][col] == target) 
+            {
+                return true;
+            } 
+            else if (matrix[row][col] > target) {
+                row--; // Move up
+            } 
+            else 
+            {
+                col++; // Move right
+            }
+        }
+        return false;
     }
 }
