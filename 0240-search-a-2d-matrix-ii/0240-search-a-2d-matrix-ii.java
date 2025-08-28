8b310f0de1bba7fbe64d7f8badb8657fdf74e276
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2025-08-28 19:54:27 +0530

    Time: 6 ms (48.88%), Space: 45.9 MB (82.79%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,14 +1,59 @@
 class Solution {
+    public static boolean binarySearch(int[] matrix, int target){
+
+        // Naive Solution  - TC - O(N * M), SC- O(1)
+        // int n = matrix.length;
+        // int m = matrix[0].length;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(matrix[i][j]== target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        //Optimal Solution TC - O(N * logM), SC - O(1)
+        int n = matrix.length;
+        int left =0, right = n-1;
+        while(left<=right){
+            int mid = (left + right)/2;
+            if(matrix[mid] == target){
+                return true;
+            }
+            if(matrix[mid]<target){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return false;
+    }
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
         int m = matrix[0].length;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]== target){
-                    return true;
-                }
-            }
+            boolean ans =  binarySearch(matrix[i],target);
+            if (ans == true)
+                return true;
         }
         return false;
+
+
+        //2nd Optimal Solution  TC - O(N + M), SC - O(1) - efficient code
+        // int row = 0;
+        // int col = matrix[0].length - 1;
+
+        // while (row < matrix.length && col >= 0) {
+        //     if (matrix[row][col] == target) {
+        //         return true;
+        //     } else if (matrix[row][col] > target) {
+        //         col--; // Move left
+        //     } else {
+        //         row++; // Move down
+        //     }
+        // }
+        // return false;
     }
 }
