a974cf53de417ff5953ce5f7acdacc6f7a69577e
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2025-09-07 00:10:39 +0530

    Time: 31 ms (51.35%), Space: 55.4 MB (54.1%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -1,21 +1,38 @@
 class StockSpanner {
     //Brute Force - TC- O(N) where N = number of days, SC- O(number of next calls)
-    ArrayList<Integer> list;
+    // ArrayList<Integer> list;
 
-    public StockSpanner() {
-        list = new ArrayList<Integer>();
-    }
+    // public StockSpanner() {
+    //     list = new ArrayList<Integer>();
+    // }
     
+    // public int next(int price) {
+    //     list.add(price);
+    //     int count=1;
+    //     for(int i=list.size()-2;i>=0;i--){
+    //         if(list.get(i) <= price)
+    //             count++;
+    //         else
+    //             break;
+    //     }
+    //     return count;
+    // }
+
+    //Optimal - TC O(N),SC- O(N)
+    Stack<int[]> st; 
+    int index;
+    public StockSpanner() {
+        index = -1;
+        st = new Stack<>();
+    }
     public int next(int price) {
-        list.add(price);
-        int count=1;
-        for(int i=list.size()-2;i>=0;i--){
-            if(list.get(i) <= price)
-                count++;
-            else
-                break;
+        index += 1;
+        while(!st.isEmpty() && st.peek()[0] <= price){
+          st.pop();
         }
-        return count;
+        int ans = index - (st.isEmpty() ? -1 : st.peek()[1]);
+        st.push(new int[]{price, index}); 
+        return ans;
     }
 }
 
