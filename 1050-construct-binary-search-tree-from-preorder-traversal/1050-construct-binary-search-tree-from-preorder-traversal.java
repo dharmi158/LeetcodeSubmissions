2f39793eefffff703a979135cd3d4bcc80f3c7f6
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2025-11-12 13:46:32 +0530

    Time: 0 ms (100%), Space: 43.4 MB (8.18%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
@@ -16,20 +16,35 @@
 class Solution {
     TreeNode root = null;
     public TreeNode bstFromPreorder(int[] preorder) {
-        for(int val : preorder){
-            root = insertIntoBST(root,val);
-        }
-        return root;
+        // for(int val : preorder){
+        //     root = insertIntoBST(root,val);
+        // }
+        // return root;
+
+        //eficient code Tc - o(n)
+        return build(preorder, Integer.MAX_VALUE);
+
     }
-    private TreeNode insertIntoBST(TreeNode root, int val){
-        if (root == null)
-            return new TreeNode(val);
+    // private TreeNode insertIntoBST(TreeNode root, int val){
+    //     if (root == null)
+    //         return new TreeNode(val);
 
-        if (val < root.val)
-            root.left = insertIntoBST(root.left, val);
-        else if (val > root.val)
-            root.right = insertIntoBST(root.right, val);
+    //     if (val < root.val)
+    //         root.left = insertIntoBST(root.left, val);
+    //     else if (val > root.val)
+    //         root.right = insertIntoBST(root.right, val);
 
+    //     return root;
+    // }
+
+    int index = 0;
+    private TreeNode build(int[] preorder, int bound){
+        if(index == preorder.length || preorder[index] > bound){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[index++]);
+        root.left = build(preorder, root.val);
+        root.right = build(preorder, bound);
         return root;
     }
 }
