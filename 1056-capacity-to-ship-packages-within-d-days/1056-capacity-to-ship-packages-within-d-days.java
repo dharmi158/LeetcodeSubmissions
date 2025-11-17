076bc17421c792d9d4852d2bf211169c9e1a089c
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2025-11-17 17:18:55 +0530

    Time: 11 ms (75.44%), Space: 49.9 MB (19.83%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,39 @@
+class Solution {
+    // Binary Search - TC-o(N log(sum(weight))) , SC - O(1)
+    public int shipWithinDays(int[] weights, int days) {
+        int low  = 0;
+        int high = 0;
+
+        for(int w:weights){
+            low = Math.max(low,w); // max package weight
+            high +=w; // sum of all weights
+        }
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int needed = daysNeeded(weights, mid);
+
+            if (needed <= days) { //If a capacity mid requires less days than allowed
+                high = mid - 1;
+            } else { //If a capacity mid requires more days than allowed
+                low = mid + 1; 
+            }
+        }
+        return low; // smallest valid capacity
+    }
+
+    private int daysNeeded(int[] weights, int cap) {
+        int currentWeight = 0;
+        int totalDays = 1; // at least one day needed
+
+        for (int w : weights) {
+            if (currentWeight + w > cap) {
+                totalDays++;
+                currentWeight = w; 
+            } else {
+                currentWeight += w;
+            }
+        }
+        return totalDays;
+    }
+}
