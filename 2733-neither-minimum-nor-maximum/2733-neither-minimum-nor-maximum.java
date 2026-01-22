be4eceeba7ca7a7e92e50ff7a986603bbc3701c9
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2026-01-22 11:33:20 +0530

    Time: 4 ms (100%), Space: 47.1 MB (55.07%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
@@ -1,26 +1,29 @@
 class Solution {
     public int findNonMinOrMax(int[] nums) {
         if(nums.length<=2) return -1;
+        
+        // TC - O(N) , SC - O(1) - using min max scan 
+        int max=Integer.MIN_VALUE,min= Integer.MAX_VALUE;
 
-        // int max=Integer.MIN_VALUE,min= Integer.MAX_VALUE;
-
-        // for(int i=0; i<nums.length; ++i){
-        //     if(nums[i]> max) max= nums[i];
-        //     if(nums[i]<min) min= nums[i];
-        // }
-
-        // for(int i=0; i<nums.length; ++i){
-        //     if(nums[i]!= max && nums[i] != min) return nums[i];
-        // }
-        // return -1;
-
-        else {
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-            for(int x : nums) {
-                pq.add(x);
-            }
-            pq.poll();
-            return pq.peek();
+        for(int i=0; i<nums.length; ++i){
+            if(nums[i]> max) max= nums[i];
+            if(nums[i]<min) min= nums[i];
         }
+
+        for(int i=0; i<nums.length; ++i){
+            if(nums[i]!= max && nums[i] != min) return nums[i];
+        }
+        return -1;
+
+
+        // TC - O(N logN) , SC - O(N) - using priority queue
+        // else {
+        //     PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //     for(int x : nums) {
+        //         pq.add(x);
+        //     }
+        //     pq.poll();
+        //     return pq.peek();
+        // }
     }
 }
