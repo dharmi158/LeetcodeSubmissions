9d8fa7d37c4bdad7544814fde9bba461389db2f3
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2026-01-22 11:29:57 +0530

    Time: 15 ms (6.24%), Space: 47.1 MB (55.07%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
@@ -2,16 +2,25 @@ class Solution {
     public int findNonMinOrMax(int[] nums) {
         if(nums.length<=2) return -1;
 
-        int max=Integer.MIN_VALUE,min= Integer.MAX_VALUE;
+        // int max=Integer.MIN_VALUE,min= Integer.MAX_VALUE;
 
-        for(int i=0; i<nums.length; ++i){
-            if(nums[i]> max) max= nums[i];
-            if(nums[i]<min) min= nums[i];
-        }
+        // for(int i=0; i<nums.length; ++i){
+        //     if(nums[i]> max) max= nums[i];
+        //     if(nums[i]<min) min= nums[i];
+        // }
 
-        for(int i=0; i<nums.length; ++i){
-            if(nums[i]!= max && nums[i] != min) return nums[i];
+        // for(int i=0; i<nums.length; ++i){
+        //     if(nums[i]!= max && nums[i] != min) return nums[i];
+        // }
+        // return -1;
+
+        else {
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for(int x : nums) {
+                pq.add(x);
+            }
+            pq.poll();
+            return pq.peek();
         }
-        return -1;
     }
 }
