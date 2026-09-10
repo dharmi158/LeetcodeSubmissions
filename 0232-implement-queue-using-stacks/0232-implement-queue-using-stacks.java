283d52e983acaf305daf18d4e4effcb9e943eb9a
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2026-09-10 13:10:40 -0400

    Time: 0 ms (100%), Space: 42.9 MB (15.03%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,70 +1,32 @@
 class MyQueue {
-    private Stack<Integer> s1;
-    private Stack<Integer> s2;
+    Stack<Integer> inSt;
+    Stack<Integer> outSt;
 
     public MyQueue() {
-        s1 = new Stack<>();
-        s2 = new Stack<>();
+        this.inSt = new Stack<>();
+        this.outSt = new Stack<>();
     }
     
     public void push(int x) {
-        // while(!s1.isEmpty()){
-        //     s2.push(s1.pop());
-        // }
-        // s1.push(x);
-        // while(!s2.isEmpty()){
-        //     s1.push(s2.pop());
-        // }
-
-        //Approach - 2 
-        s1.push(x);
+        inSt.push(x);
     }
     
     public int pop() {
-        //return s1.pop();
-
-        // Approach - 2
-        if(!s2.isEmpty()){
-            return s2.pop();
-        }
-        else{
-            while(!s1.isEmpty()){
-                s2.push(s1.pop());
-                //s1.pop();
-            }
-            return s2.pop();
-        }
+        if(empty()) return -1;
+        peek();
+        return outSt.pop();
     }
     
     public int peek() {
-        //return s1.peek();
-
-        //Approach - 2 
-        if(!s2.isEmpty()){
-            return s2.peek();
-        }
-        else{
-            while(!s1.isEmpty()){
-                s2.push(s1.pop());
-                //s1.pop();
+        if(outSt.isEmpty()){
+            while(!inSt.isEmpty()){
+                outSt.push(inSt.pop());
             }
-            return s2.peek();
         }
+        return outSt.peek();
     }
     
     public boolean empty() {
-        //return s1.isEmpty()
-
-        //Approach - 2
-        return s1.isEmpty() && s2.isEmpty();
+        return inSt.isEmpty() && outSt.isEmpty();
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
